297c3d066ca7bc0bcdd1f42ee5fb0bf9c741a495
R-Jahan <thejahan50@gmail.com>
2025-10-28 18:28:06 +0600

    images

--- js/app.js
+++ js/app.js
@@ -3,11 +3,11 @@ let offBtn = document.querySelector('.offBtn');
 let blub = document.querySelector('.blub');
 
 function turnOnBlub(){
-    blub.src="/asscet/image/onBlub.png"
+    blub.src='./image/onBlub.png'
 }
 
 function turnOffBlub(){
-    blub.src="/asscet/image/offBlub.png"
+    blub.src="./image/offBlub.png"
 }
 
 onBtn.addEventListener("click", turnOnBlub);
